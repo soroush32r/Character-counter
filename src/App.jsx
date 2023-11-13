e692c02bd9e3454ce1eb952fb3a698d8f83381b5
Soroush <ranjbar.soroush32@gmail.com>
2023-11-13 13:41:13 +0330

    Character state is created and with handleCharaterChange handler character will updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 
 function App() {
+  const [character,setCharacter] = useState('')
+
+  const handleCharacterChange = ({ target }) => {
+    setCharacter(target.value)
+  }
+
   return (
     <div>
-
+      <textarea value={character} onChange={handleCharacterChange} />
+      <h1>Character Count: {character.length}</h1>
     </div>
   )
 }
